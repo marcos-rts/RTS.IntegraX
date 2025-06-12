66ecfb6fd218cda0c6180e945e49a2194d86ea6b
Marcos Santos - Net Turbo <50344569+marcos-rts@users.noreply.github.com>
2025-06-12 10:29:36 -0300

    refactor: ajusta caminho de importação do módulo de banco de dados

--- src/modules/services/controle_de_itens/backend/routes/itens.route.js
+++ src/modules/services/controle_de_itens/backend/routes/itens.route.js
@@ -5,7 +5,7 @@
 
 const express = require('express');
 const router = express.Router();
-const db = require('./../../../../../config/database');
+const db = require('./../config/database');
 const Logger = require('./../../../../utils/Console_Logger');
 const logger = new Logger();
 
