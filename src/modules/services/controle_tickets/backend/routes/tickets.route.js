0b5a7a42e5bfcc1a5b6edc342d1c4366226a1f7f
Marcos Santos - Net Turbo <50344569+marcos-rts@users.noreply.github.com>
2025-06-17 17:47:29 -0300

    feat: adiciona rota protegida com middleware de autenticação

--- src/modules/services/controle_tickets/backend/routes/tickets.route.js
+++ src/modules/services/controle_tickets/backend/routes/tickets.route.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const ticketController = require('../controller/ticket.controller');
+const authMiddleware = require('./../../../../auth/auth.middleware')
+
+router.get('/protegido', authMiddleware, (req, res) => {
+    res.json({ message: `Você está autenticado como ${req.usuario.email}` });
+});
 
 router.get('/', ticketController.getTickets);
 router.post('/', ticketController.createTicket);
