b71071227ae0bf28c7359d537146d19dfb18d0e0
Marcos Santos - Net Turbo <50344569+marcos-rts@users.noreply.github.com>
2025-06-18 12:23:36 -0300

    feat: adiciona middleware de autenticação nas rotas de usuários e validação de token

--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { login, register, users } = require('./auth.controller');
+const authMiddleware = require('./auth.middleware');
 
 router.post('/login', login);
 router.post('/register', register);
-router.get('/users', users);
+router.get('/users', authMiddleware, users);
+
+router.get('/validate_token', authMiddleware, (req, res) => {
+    res.json({ message: `Você está autenticado como ${req.usuario.email}` });
+});
 
 module.exports = router;
