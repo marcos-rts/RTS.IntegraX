9fce0280e7dfe3a3b170c64361cabbedd20bbcbb
Marcos Santos - Net Turbo <50344569+marcos-rts@users.noreply.github.com>
2025-07-28 13:43:27 -0300

    refactor: refatoração da rota de status e remoção do módulo rts.grupo_status

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const Logger = require('../modules/utils/Console_Logger');
 const FileLogger = require('../modules/utils/FileLogger');
+const authMiddleware = require('../modules/auth/auth.middleware');
 
 const fileLogger = new FileLogger('logs/app.log', 'logs/app.json');
 const logger = new Logger();
@@ -12,8 +13,8 @@ const rotasAutenticacao = require('../modules/auth/auth.route');
 router.use('/autenticacao', rotasAutenticacao);
 
 // ⚙️ Grupo e Status
-const rotasGrupoStatus = require('./rts.grupo_status');
-router.use('/rts', rotasGrupoStatus);
+const {status} = require('../controller/rts.grupo_status');
+router.get('/status', authMiddleware, status);
 
 // ⚙️ Módulo Tickets
 try {
--- src/routes/rts.grupo_status.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { status } = require('../controller/rts.grupo_status');
-const authMiddleware = require('../modules/auth/auth.middleware');
-
-router.get('/status', authMiddleware, status);
-
-module.exports = router;
