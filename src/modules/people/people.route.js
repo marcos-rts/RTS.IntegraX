6c42fe971d6542624a920dc92fe68594167761d5
Marcos Alexandre <marcos.alex1002@gmail.com>
2025-09-03 23:46:38 -0300

    feat: Adição de chamada para criação de pessoa

--- src/modules/people/people.route.js
+++ src/modules/people/people.route.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { pessoa } = require('./people.controller');
+const { pessoa, createPessoa } = require('./people.controller');
 const authMiddleware = require('../auth/auth.middleware');
 
 router.get('/', authMiddleware, pessoa);
-
+router.post('/createPessoa', authMiddleware, createPessoa);
 module.exports = router;
