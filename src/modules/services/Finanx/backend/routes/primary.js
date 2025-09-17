6063ddccfc033ccf348fff8eaedb05b58cea3a6a
Marcos Santos - Net Turbo <50344569+marcos-rts@users.noreply.github.com>
2025-09-17 14:25:55 -0300

    feat: alterado nomeclatura de controller de transacao e suas chamadas

--- src/modules/services/Finanx/backend/routes/primary.js
+++ src/modules/services/Finanx/backend/routes/primary.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const finanxController = require('../controller/primary');
+const fxTransacao = require('../controller/transacao.controller');
 const authMiddleware = require('./../../../../auth/auth.middleware')
 
 router.use(authMiddleware);
 
 
-router.post("/transacoes", finanxController.createTransacao);
-router.get("/transacoes", finanxController.getAllTransacoes);
-router.get("/transacoes/:id", finanxController.getTransacaoById);
-router.put("/transacoes/:id", finanxController.updateTransacao);
-router.delete("/transacoes/:id", finanxController.deleteTransacao);
+router.post("/transacoes", fxTransacao.createTransacao);
+router.get("/transacoes", fxTransacao.getAllTransacoes);
+router.get("/transacoes/:id", fxTransacao.getTransacaoById);
+router.put("/transacoes/:id", fxTransacao.updateTransacao);
+router.delete("/transacoes/:id", fxTransacao.deleteTransacao);
 
 
 module.exports = router;
