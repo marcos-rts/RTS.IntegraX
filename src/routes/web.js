7a182ca35a3f3ecbbae2132fa45df48293bf4312
Marcos Santos - Net Turbo <50344569+marcos-rts@users.noreply.github.com>
2025-09-17 17:51:02 -0300

    feat: adição de pagina de adição de pessoas

--- src/routes/web.js
+++ src/routes/web.js
@@ -31,6 +31,10 @@ router.get('/pessoas', (req, res) => {
     res.sendFile(path.join(publicPath, 'users', 'pessoas.html'));
 });
 
+router.get('/nova_pessoa', (req, res) => {
+    res.sendFile(path.join(publicPath, 'users', 'nova_pessoa.html'));
+});
+
 // 🎟️ Tickets
 router.get('/lista_tickets', (req, res) => {
     res.sendFile(path.join(publicPath, 'tickets', 'lista_tickets.html'));
