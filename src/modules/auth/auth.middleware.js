b65a5e4766864ac80fb1ef05716eccb936572c62
Marcos Santos - Net Turbo Telecom <50344569+marcos-rts@users.noreply.github.com>
2025-10-27 12:47:11 -0300

    feat: atualização do middleare para melhorar no tratamento de bug

--- src/modules/auth/auth.middleware.js
+++ src/modules/auth/auth.middleware.js
@@ -1,35 +1,42 @@
-const jwt = require('jsonwebtoken');
-const db = require('../../config/database');
+const jwt = require("jsonwebtoken");
+const db = require("../../config/database");
 
 // Função para verificar se o tokens esta na blacklist
 const isTokenBlackListed = async (token) => {
-    const [rows] = await db.execute('SELECT * FROM JWT_blacklist WHERE token = ? AND expira_em > NOW()', [token]);
-    return rows.lenghth > 0;
-}
+  const [rows] = await db.execute(
+    "SELECT * FROM JWT_blacklist WHERE token = ? AND expira_em > NOW()",
+    [token]
+  );
+  return rows.length > 0;
+};
 
 const autenticar = async (req, res, next) => {
-    try {
-        const authHeader = req.headers.authorization;
+  console.log("[AUTH MIDDLEWARE] Passou:", req.originalUrl);
 
-        if (!authHeader) {
-            return res.status(401).json({ message: 'Token não fornecido.' });
-        }
+  try {
+    const authHeader = req.headers.authorization;
 
-        const token = authHeader.split(' ')[1]; // "Bearer <token>"
-
-        // Verifica se o token está na blacklist
-        const tokenNegado = await isTokenBlackListed(token);
-        if (tokenNegado) {
-            return res.status(401).json({ message: 'Token foi invalidado (logout).' });
-        }
-
-        // Verifica a validade do token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.usuario = decoded; // payload com { id, email }
-        next();
-    } catch (err) {
-        return res.status(403).json({ message: 'Token inválido ou expirado.' });
+    if (!authHeader) {
+      return res.status(401).json({ message: "Token não fornecido." });
     }
+
+    const token = authHeader.split(" ")[1]; // "Bearer <token>"
+
+    // Verifica se o token está na blacklist
+    const tokenNegado = await isTokenBlackListed(token);
+    if (tokenNegado) {
+      return res
+        .status(401)
+        .json({ message: "Token foi invalidado (logout)." });
+    }
+
+    // Verifica a validade do token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.usuario = decoded; // payload com { id, email }
+    next();
+  } catch (err) {
+    return res.status(403).json({ message: "Token inválido ou expirado." });
+  }
 };
 
 module.exports = autenticar;
